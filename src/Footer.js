88650644765cab642c935999dce7d57b83114b21
Newadventure2022 <celias.efl.esl.classes@gmail.com>
2023-03-09 13:29:48 +0100

    Updated the footer

---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <a
+        href="https://github.com/Newadventure2022/Celias-Projects-for-SheCodes"
+        target="_blank"
+        rel="noreferrer"
+        className="Visit"
+      >
+        Open-source code <span className="my-name">by Corona-Doran</span>
+      </a>
+    </div>
+  );
+}
