307a3a8bfd8edfb02f574aa2dd49cd33da99747e
Newadventure2022 <celias.efl.esl.classes@gmail.com>
2023-03-09 13:37:46 +0100

    removed the react logo image and the forecast image

--- src/Forecast.js
+++ src/Forecast.js
@@ -7,12 +7,7 @@ export default function Forecast() {
     <div className="weather-forecast" id="forecast">
       <div className="row">
         <div className="col-2">
-          <div className="weather-forecast-date">
-            <img
-              src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/067/836/original/Weather_Forecast_img.jpeg?1676508026"
-              alt="weather-week"
-            />
-          </div>
+          <div className="weather-forecast-date"></div>
         </div>
       </div>
     </div>
