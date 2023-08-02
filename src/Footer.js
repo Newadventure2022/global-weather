b8148edaf06ff2800ba89738bf31a3b781d7e956
Newadventure2022 <celias.efl.esl.classes@gmail.com>
2023-08-02 17:39:22 +0200

    Updated the footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,13 +4,24 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <div className="Footer">
+      Created by{" "}
       <a
-        href="https://github.com/Newadventure2022"
+        href="https://celia-corona-doran2023.netlify.app/"
         target="_blank"
         rel="noreferrer"
-        className="Visit"
+        className="footer-links"
       >
-        Open-source code <span className="my-name">by Celia Corona-Doran</span>
+        {" "}
+        <span className="my-name">Celia Corona-Doran</span>{" "}
+      </a>{" "}
+      and open-sourced on
+      <a
+        href="https://github.com/Newadventure2022/Celias-Projects-for-SheCodes/blob/main/code.js"
+        target="_blank"
+        rel="noreferrer"
+        className="footer-links"
+      >
+        <span className="GitHub"> GitHub</span>
       </a>
     </div>
   );
